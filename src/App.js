a2a6941a855d82b6e22ee59ac179fb6a0bb1dbdf
SanketMhaske1 <sanketmhaske005@gmail.com>
2024-06-13 19:52:43 +0530

    Second Commit Of React Project 7

--- src/App.js
+++ src/App.js
@@ -1,3 +1,14 @@
+import React from "react";
+import Heading from "./Components/Heading";
+import Blog from "./Components/Blog";
+import Pagination from "./Components/Pagination";
+
 export default function App() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <Heading />
+      <Blog />
+      <Pagination />
+    </div>
+  );
 }
--- src/Components/Heading.js
+++ src/Components/Heading.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Heading() {
-  return <div>Heading</div>;
+  return (
+    <div>
+      <h1>CodeHelp blogs</h1>
+    </div>
+  );
 }
 
 export default Heading;
