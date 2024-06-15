137b302bf44c407f4ee39d1122f7e4b913a669d7
SanketMhaske1 <sanketmhaske005@gmail.com>
2024-06-15 20:14:30 +0530

    Fifth Commit Of React Project 7

--- src/Components/Spinner.js
+++ src/Components/Spinner.js
@@ -2,7 +2,11 @@ import React from "react";
 import "./Spinner.css";
 
 function Spinner() {
-  return <div className="spinner"></div>;
+  return (
+    <div className=" w-full h-[100vh] flex justify-center items-center">
+      <div className="spinner"></div>
+    </div>
+  );
 }
 
 export default Spinner;
